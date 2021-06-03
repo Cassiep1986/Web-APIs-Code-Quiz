d772fbb4898330a9102903c7cf3eeb09c286156d
seacrest <cassiep1986@gmail.com>
2021-06-02 19:00:42 -0700

    added start button and main page text

---
+++ script.js
@@ -0,0 +1,8 @@
+var startquizbtn = document.querySelector("#quizstart");
+var startquizbtn = document.querySelector("#startquiz-btn");
+var startquizbtn = document.querySelector("#quiz");
+
+startquizbtn.addEventListener("click", function(e) {
+quizstart.classlist.add("hide");
+quiz.classlist.remove("hide");
+})
