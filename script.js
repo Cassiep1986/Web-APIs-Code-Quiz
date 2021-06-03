8b93cdf9c723d561e4beb55d3d7d48ed4d3005ec
seacrest <cassiep1986@gmail.com>
2021-06-03 00:46:36 -0700

    added questions/answers

--- script.js
+++ script.js
@@ -3,20 +3,91 @@ var startbtn = document.querySelector("#start-btn");
 var quizcontent = document.querySelector("#quiz-content");
 var question = document.querySelector("#question");
 var choices = document.querySelector("#choices");
+var timer = document.getElementById("time");
 
-var question1 = [
-    {
+
+var question1 = {
+    
         text:"What is a my favorite color?",
         correctAnswer:"Pink",
         options: ["Pink", "Yellow", "Blue"],
-    },
-];
+    }
 
+var question2 = {
+    
+        text:"What is a my favorite Animal?",
+        correctAnswer:"Unicorn",
+        options: ["Unicorn", "Zebra", "Tiger"],
+    }
+
+var question3 = {
+    
+        text:"What color is the sky?",
+        correctAnswer:"Blue",
+        options: ["Green", "Blue", "Banana"],
+    }
+
+var question4 = {
+    
+        text:"Question 4?",
+        correctAnswer:"Right",
+        options: ["Wrong", "Wrong", "Right"],
+    }
+
+var question5 = {
+    
+        text:"Question 5?",
+        correctAnswer:"Right",
+        options: ["Wrong", "Wrong", "Right"],
+    }
+
+function quiztimer(){
+    var timeleft = 75;
+    var timeinterval = setInterval(function(){
+        if (timeleft > 1) { 
+            timer.textContent = timeleft;
+            timeleft--;       
+        } else {
+            clearInterval(timeinterval);
+        }
+     },1000);
+}
+
+
+function populatequestion1() {
+    var q1 = document.createElement("h3");
+    q1.innerHTML = question1.text;
+    document.getElementById("question").append(q1);
+}
+
+function populatechoices(x) {
+    var choicebtn = document.createElement("button");
+    choicebtn.innerHTML = x;
+    document.getElementById("choices").append(choicebtn);
+}
+
+for (var i = 0; i < question1.options.length; i++) {
+    populatechoices(question1.options[i]) 
+    
+}
+
+function populatequestion2() {
+    var q2 = document.createElement("h3");
+    q2.innerHTML = question2.text;
+    document.getElementById("question").append(q2);
+}
+
+
+
+populatequestion1()
 
 startbtn.addEventListener("click", function (e) {
-quizprompt.classList.add("hide");
-quizcontent.classList.remove("hide")
-})
+    quizprompt.classList.add("hide");
+    quizcontent.classList.remove("hide");
+});
 
-
-
+choices.addEventListener("click", function (e) {
+if (question1.correctAnswer) {
+  console.log("okay")    
+}
+});
