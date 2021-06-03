622dc68aab50f05434233f9caf7e1eb7c5fbe3b3
seacrest <cassiep1986@gmail.com>
2021-06-02 19:37:16 -0700

    added 1st ?

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-var startquizbtn = document.querySelector("#quizstart");
-var startquizbtn = document.querySelector("#startquiz-btn");
-var startquizbtn = document.querySelector("#quiz");
+var quizprompt = document.querySelector("#quiz-prompt");
+var startbtn = document.querySelector("#start-btn");
+var quizcontent = document.querySelector("#quiz-content");
+var question = document.querySelector("#question");
+var choices = document.querySelector("#choices");
 
-startquizbtn.addEventListener("click", function(e) {
-quizstart.classlist.add("hide");
-quiz.classlist.remove("hide");
+var question1 = [
+    {
+        text:"What is a my favorite color?",
+        correctAnswer:"Pink",
+        options: ["Pink", "Yellow", "Blue"],
+    },
+];
+
+
+startbtn.addEventListener("click", function (e) {
+quizprompt.classList.add("hide");
+quizcontent.classList.remove("hide")
 })
+
+
+
