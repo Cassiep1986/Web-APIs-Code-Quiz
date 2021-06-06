752a156ee346d387e8f9fdf269f2b1b7c01045be
seacrest <cassiep1986@gmail.com>
2021-06-06 13:47:54 -0700

    committing in order to revert

--- script.js
+++ script.js
@@ -4,89 +4,42 @@ var quizcontent = document.querySelector("#quiz-content");
 var question = document.querySelector("#question");
 var choices = document.querySelector("#choices");
 var timer = document.getElementById("time");
-var maxquestions = 5;
 
-
-var questionSet = [ {
+var question1 = {
 
         text:"What is a my favorite color?",
-        correctAnswer:0,
+        correctAnswer:"pink",
         options: ["Pink", "Yellow", "Blue"],
-    },
+    }    
 
-{
+var question2 = {       
         text:"What is a my favorite Animal?",
         correctAnswer:"Unicorn",
         options: ["Unicorn", "Zebra", "Tiger"],
-    },
+    }
 
-{
+
     
-        text:"What color is the sky?",
-        correctAnswer:"Blue",
-        options: ["Green", "Blue", "Banana"],
-    },
-
-{
-    
-        text:"Question 4?",
-        correctAnswer:"Right",
-        options: ["Wrong", "Wrong", "Right"],
-    },
-
-{
-    
-        text:"Question 5?",
-        correctAnswer:"Right",
-        options: ["Wrong", "Wrong", "Right"],
-    } ]
-
-function quiztimer(){
-    var timeleft = 75;
-    var timeinterval = setInterval(function(){
-        if (timeleft > 1) { 
-            timer.textContent = timeleft;
-            timeleft--;       
-        } else {
-            clearInterval(timeinterval);
-        }
-     },1000);
-}
-
-
 function populatequestion1() {
     var q1 = document.createElement("h3");
     q1.innerHTML = question1.text;
     document.getElementById("question").append(q1);
 }
 
+for (var i = 0; i < question1.options.length; i++) {
+ 
 function populatechoices(x) {
     var choicebtn = document.createElement("button");
-    choicebtn.innerHTML = x;
+    choicebtn.innerHTML = [x];
     document.getElementById("choices").append(choicebtn);
-}
-
-for (var i = 0; i < question1.options.length; i++) {
-    populatechoices(question1.options[i]) 
-    
-}
-
-function populatequestion2() {
-    var q2 = document.createElement("h3");
-    q2.innerHTML = question2.text;
-    document.getElementById("question").append(q2);
-}
+}}
 
 
+    startbtn.addEventListener("click", function (e) {
+        quizprompt.classList.add("hide");
+        quizcontent.classList.remove("hide");
+        populatequestion1()
+        populatechoices(options)
+    });
 
-populatequestion1()
 
-startbtn.addEventListener("click", function (e) {
-    quizprompt.classList.add("hide");
-    quizcontent.classList.remove("hide");
-});
-
-choices.addEventListener("click", function (e) {
-if (e.target.matches("Pink")); {
-}
-});
